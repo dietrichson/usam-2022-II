4262fa45bb9a1ccba7acbd272edf7c03e7af5238
Aleksander Dietrichson <dietrichson@gmail.com>
2022-09-05 18:48:08 +0000

    commiteando el momento antes del refactoring.

--- connect-to-google-sheet.R
+++ connect-to-google-sheet.R
@@ -6,5 +6,9 @@ if(!require(googlesheets4)){
 
 
 gs4_deauth()
+
 my_url <- "https://docs.google.com/spreadsheets/d/1SF5DDTEfbQFSNjvYKQnsQKdsBdE7oJeTywCk92pB7f8/edit?usp=sharing"
-my_data <- googlesheets4::read_sheet(my_url)
+
+my_url2 <- 'https://docs.google.com/spreadsheets/d/1SF5DDTEfbQFSNjvYKQnsQKdsBdE7oJeTywCk92pB7f8/edit#gid=1964221805'
+  
+my_data <- googlesheets4::read_sheet(my_url2)
